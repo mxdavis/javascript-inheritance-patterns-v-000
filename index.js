4131feaf213ba93336ff68189fde034144464703
mxdavis <md01@me.com>
2017-06-08 13:15:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const app = "I don't do much."
+//Shape Constructor
+function Shape(sides, x, y) {
+  this.sides = sides;
+  this.x = x;
+  this.y = y;
+}
+
+// Rectangle constructor
+function Rectangle(sides, width, height) {
+  this.sides = sides;
+  this.width = width;
+  this.height = height;
+}
+Rectangle.prototype.area = function() {
+  return this.width * this.height;
+}
+Rectangle.prototype.perimeter = function () {
+  return (this.width + this.height) * 2;
+}
+ 
+// Square constructor
+function Square(sides, length) {
+  Rectangle.call(this, sides, length, length);
+}
+ 
+var rect = new Rectangle(4, 3, 5);
+var square = new Square(4, 2);
+ 
+rect.area();
+square.area();
+ 
+Rectangle.prototype.internalAngles = 90;
+rect.internalAngles;
+square.internalAngles;
